cccb43b5d47390d51680126ae283040aac7a4f22
paekjiyeon <qor3127@naver.com>
2019-07-24 04:15:48 +0900

    can see image index detail

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='media/images/', blank = True)
+    # image = models.ImageField(upload_to='media/images/', blank = True)
     # file = models.FileField(null=True)
+    image = ProcessedImageField(
+            upload_to='media/images/', # 저장 위치
+            processors=[ResizeToFill(600,600)], # 처리할 작업 목록
+            format='JPEG', # 저장 포맷(확장자)
+            options= {'quality': 90 }, # 저장 포맷 관련 옵션 (JPEG 압축률 설정)
+            blank = True,
+    )
     body = models.TextField()
 
     def __str__(self):
